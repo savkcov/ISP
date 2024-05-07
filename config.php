c3405cabcbf7f81231d821567b1ad5fa122fd4d2
unknown <savckovalexei@yandex.ru>
2024-05-07 18:55:40 +0300

    изменение параметров подключения к БД

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 // константы:
 define ('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
 define ('SITE_PATH', realpath(dirname(__FILE__)).DS); // путь к корневой папке сайта
-define("db_host","localhost");// хост базы данных
+define("db_host","sql11.freemysqlhosting.net");// хост базы данных
 define("db_port","3306"); // порт для базы данных
-define("db_user","root"); //имя пользователя базы данных
-define("db_password",""); // пароль к базе данных
-define("db_database","ism");// имя базы данных
+define("db_user","sql11704781"); //имя пользователя базы данных
+define("db_password","jtnlst7HRf"); // пароль к базе данных
+define("db_database","sql11704781");// имя базы данных
 define('SiteName','Сайт ISM'); //название сайта
 define('CLASS_PATH', SITE_PATH . 'classes' . DS); //путь до файлов классов
 define('MODEL_PATH', SITE_PATH . 'models' . DS); //путь до файлов моделей
